ff95ead001b28c36b35db7b4cad3b0b4875fc3a1
jgertos9809 <jgertos.9809@umak.edu.ph>
2026-08-26 14:40:10 +0800

    Activity1
    bossing walang kanin

---
+++ disa/gertos.java
@@ -0,0 +1,13 @@
+package disa;
+public class gertos {
+    public static void main(String[] args) {
+    	
+        System.out.println("Full Name: Alex Santos");
+        System.out.println("Age: 20");
+        System.out.println("Course and Year Level: BS Information Technology, 2nd Year");
+        System.out.println("School: Technological Institute of the Philippines");
+        System.out.println("Hobbies: PC Gaming, Digital Art, Reading Sci-Fi");
+        System.out.println("Favorite Programming Language or Technology: Python");
+        System.out.println("Reason why I want to learn Java: I want to master object-oriented programming to build scalable backend systems and Android applications.");
+    }
+}
