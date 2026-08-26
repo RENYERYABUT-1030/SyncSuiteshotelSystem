8cb7b02076bc0261704f732a7c7ca8a2a23088d1
RENYERRRR YABUT <yabutrenyer30@gmail.com>
2026-08-26 15:29:49 +0800

    Update

--- disa/gertos.java
+++ disa/gertos.java
@@ -1,11 +1,11 @@
-package disa;
+
 public class gertos {
     public static void main(String[] args) {
     	
-        System.out.println("Full Name: Alex Santos");
-        System.out.println("Age: 20");
+        System.out.println("Full Name: Joshua Miguel Gertos");
+        System.out.println("Age: 21");
         System.out.println("Course and Year Level: BS Information Technology, 2nd Year");
-        System.out.println("School: Technological Institute of the Philippines");
+        System.out.println("School: University of Makati (UMak)");
         System.out.println("Hobbies: PC Gaming, Digital Art, Reading Sci-Fi");
         System.out.println("Favorite Programming Language or Technology: Python");
         System.out.println("Reason why I want to learn Java: I want to master object-oriented programming to build scalable backend systems and Android applications.");
